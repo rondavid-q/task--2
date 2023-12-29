32cb7a0bcbe024d05710e6e23263d5a765ca52f1
ronald <ronalddavidofc@gmail.com>
2023-12-29 08:18:42 -0800

    added bucket creation

--- main.tf
+++ main.tf
@@ -136,6 +136,5 @@ resource "aws_route_table_association" "private_association2" {
 #Creation of ECR to push images
 resource "aws_ecr_repository" "my_repository" {
   name = "node-service-1"
-  image_tag_mutability = "MUTABLE" 
-  scan_on_push         = true       
+  image_tag_mutability = "MUTABLE"    
 }
